010768281ad411ab863906d8d2d8c61e9d575996
Ronald Velasco <155396751+RonVel4304@users.noreply.github.com>
2024-07-03 11:51:49 -0700

    Update main.py
    Accidentally left some ex code using an online python ide...

--- main.py
+++ main.py
@@ -84,8 +84,3 @@ if __name__ == '__main__':
         if user_loop == 'No' or user_loop == 'no':
             break
 
-
-a = int(input('Enter 1st number: '))
-b = int(input('Enter 2nd number: '))
-
-print(f'Sum of {a} and {b} is {sum(a, b)}')
