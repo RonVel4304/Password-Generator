771fd3f12bbca3318264c10ddb9b2fd73a805f06
Ronald Velasco <155396751+RonVel4304@users.noreply.github.com>
2024-07-10 16:22:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import random
 
 
 def password_append(difficulty, adjective, nouns, number1, number2, special1, special2):
+    result_append = ""
     if difficulty == 4:  # Extreme password with special case at start and random capitalization
-        result = special1 + adjective + nouns + number1 + number2 + special2
+        result_append = special1 + adjective + nouns + number1 + number2 + special2
     if difficulty == 3:  # Extreme password with special case in between adj and noun along with random capitalization
-        result = adjective + special1 + nouns + number1 + number2 + special2
+        result_append = adjective + special1 + nouns + number1 + number2 + special2
     if difficulty == 2:  # Hard password with special case at the end and caps adj and noun
-        result = adjective + nouns + number1 + number2 + special1
+        result_append = adjective + nouns + number1 + number2 + special1
     if difficulty == 1:  # Easy password with cap adj and noun
-        result = adjective + nouns + number1 + number2
-    return result
+        result_append = adjective + nouns + number1 + number2
+    return result_append
 
 
-def random_caps(str):
+def random_caps(string):
     result = ''
-    for i in str:
+    for i in string:
         if random.choice([True, False]):
             result += i.upper()
         else:
@@ -23,19 +24,19 @@ def random_caps(str):
     return result
 
 
-def pass_extr(pass_adjective, pass_nouns, pass_number1, pass_number2, pass_special, pass_special2):
+def pass_extreme(pass_adjective, pass_nouns, pass_number1, pass_number2, pass_special, pass_special2):
     start_or_center_choice = [0, 1]
     start_or_center = random.choice(start_or_center_choice)
     pass_adjective = random_caps(pass_adjective)
     pass_nouns = random_caps(pass_nouns)
     if start_or_center == 0:
-        extr_password = password_append(4, pass_adjective, pass_nouns, pass_number1, pass_number2, pass_special,
-                                        pass_special2)
-        return extr_password
+        extreme_password = password_append(4, pass_adjective, pass_nouns, pass_number1, pass_number2, pass_special,
+                                           pass_special2)
+        return extreme_password
     elif start_or_center == 1:
-        extr_password = password_append(3, pass_adjective, pass_nouns, pass_number1, pass_number2, pass_special,
-                                        pass_special2)
-        return extr_password
+        extreme_password = password_append(3, pass_adjective, pass_nouns, pass_number1, pass_number2, pass_special,
+                                           pass_special2)
+        return extreme_password
 
 
 def password_generate(choice):
@@ -52,20 +53,20 @@ def password_generate(choice):
     pass_special = random.choice(special)
     pass_special2 = random.choice(special)
     if choice == 2:  # Extreme Password
-        extr_password = pass_extr(pass_adjective, pass_nouns, pass_number1, pass_number2, pass_special,
-                                  pass_special2)
-        return extr_password
+        extreme_password = pass_extreme(pass_adjective, pass_nouns, pass_number1, pass_number2, pass_special,
+                                        pass_special2)
+        return extreme_password
     elif choice == 1:  # Hard Password
         hard_password = password_append(2, pass_adjective, pass_nouns, pass_number1, pass_number2, pass_special, '')
         return hard_password
-    elif choice == 0:  # Easy Pasword
+    elif choice == 0:  # Easy Password
         return password_append(1, pass_adjective, pass_nouns, pass_number1, pass_number2, '', '')
 
 
 if __name__ == '__main__':
     while True:
         password_choices = ["extreme", "hard", "easy"]
-        loop_choices = ["Yes", "yes", "No", "no"]
+        loop_choices = ["yes", "no"]
         user_choice = str(input("Would you like to generate an Extreme, Hard, or Easy password?: "))
         user_choice = user_choice.lower()
         while user_choice not in password_choices:
@@ -80,9 +81,10 @@ if __name__ == '__main__':
             password = password_generate(0)
         print(password)
         user_loop = str(input("Would you like to generate another password? (Yes or No): "))
+        user_loop = user_loop.lower()
         while user_loop not in loop_choices:
             print("Invalid input, try again")
             user_loop = str(input("Would you like to generate another password? (Yes or No): "))
-        if user_loop == 'No' or user_loop == 'no':
+            user_loop = user_loop.lower()
+        if user_loop == 'no':
             break
-
